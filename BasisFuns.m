08ef769899c12f1e347711cdd3a9938a91e28311
Xu Yi <xuyi@ziad.cn>
2019-01-15 22:56:38 +0800

    BasisFun checked

--- BasisFuns.m
+++ BasisFuns.m
@@ -36,23 +36,25 @@ for i = 1:degree % order = degree+1;
 end
 % derivatives % the NURBS book 2nd. P61 (2.10)
 % parameter a
-Para_a = zeros(degree+1,degree+1); Para_a(1,1) = 1; % initialize. a0,0 = 1
+Para_a = zeros(degree+1,degree+1,degree+1); Para_a(1,1,:) = 1; % initialize. a0,0 = 1
 for i = 1:degree % a0,0 is initialized
     for j = 1:i+1
-        temp = knotVector( knotspanIndex-1 + degree +j-i+1 ) - knotVector( knotspanIndex-1 +j );
-        if j == 1
-            Para_a(i+1,j) = Para_a(i,1) / temp;
-        elseif j == i+1
-            Para_a(i+1,j) = - Para_a(i,i) / temp;
-        else
-            Para_a(i+1,j) = ( Para_a(i,j) - Para_a(i,j-1) ) / temp;
-        end
-        if temp == 0 % special case. a/0 = 0
-            Para_a(i+1,j) = 0;
+        for k = 1:degree+1 % a is subject to i(which in Ni)
+            temp = knotVector( knotspanIndex+k-(degree+1) + degree +j-1 -i+1 ) - knotVector( knotspanIndex+k-(degree+1) +j-1 );
+            if j == 1
+                Para_a(i+1,j,k) = Para_a(i,1,k) / temp;
+            elseif j == i+1
+                Para_a(i+1,j,k) = - Para_a(i,i,k) / temp;
+            else
+                Para_a(i+1,j,k) = ( Para_a(i,j,k) - Para_a(i,j-1,k) ) / temp;
+            end
+            if temp == 0 % special case. a/0 = 0
+                Para_a(i+1,j,k) = 0;
+            end
         end
     end
 end
-% dersBasisFunValue
+% dersBasisFunValue / calculate derivatives of Ni,degree
 ders = zeros(degree,degree+1); % initialize. [der x degree+1]
 for i = 1:degree % derivative. k<=p
     for j = 1:degree+1
@@ -67,7 +69,7 @@ for i = 1:degree % derivative. k<=p
             else
                 temp21 = N( degree+1-i,temp211 );
             end
-            temp2 = temp2 + Para_a(i+1,k)*temp21 ;
+            temp2 = temp2 + Para_a(i+1,k,j)*temp21 ;
         end
         ders(i,j) = temp1 * temp2;
     end
--- Untitled3.m
+++ Untitled3.m
@@ -2,10 +2,10 @@ clear
 clc
 
 %% varify BasisFuns
-degree = 2;
-knotVector = [0,0,0,1,2,3,4,4,5,5,5];
-u = 5/2;
-[basisFunValue, dersBasisFunValue] = BasisFuns(u, degree, knotVector);
+% degree = 2;
+% knotVector = [0,0,0,1,2,3,4,4,5,5,5];
+% u = 5/2;
+% [basisFunValue, dersBasisFunValue] = BasisFuns(u, degree, knotVector);
 
 %% Surface
 % degreeU = 2;
